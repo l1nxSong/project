5f09a0100c92bf427c499d0bb6d5ef9969c62cb2
LinxSong <2018213147@bupt.edu.cn>
2022-01-04 20:31:43 +0800

    20220104

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, render_template_string
 
 import settings
 
@@ -25,10 +25,31 @@ def welcome():  # 登陆界面/ Login
     return render_template('login.html')
 
 
-@app.route('/mainPage')
+@app.route('/mainPage', methods=['GET', 'POST'])
 def main_page():
+    if request.method == 'POST':
+        input = request.form.get('input')
+        return 'Your input is: ' + str(input)
+
     return render_template('main.html', user=currentUser)
 
+
+@app.route('/mainPage/sstiTest')
+def hello_ssti():
+    person = {
+        'name': 'hello',
+        'age': '20',
+        'password': 'abc123'
+    }
+    if request.args.get('name'):
+        person['name'] = request.args.get('name')
+    # changed
+    template = '<h2>%s</h2>' % person['name']
+    return render_template_string(template, person=person)
+
+#   http://127.0.0.1:5001/mainPage/sstiTest?name={{12345}}
+#   http://127.0.0.1:5001/mainPage/sstiTest?name={{person.password}}
+#   http://127.0.0.1:5001/mainPage/sstiTest?name={{config}}
 
 if __name__ == '__main__':
     app.run(port=5001)
